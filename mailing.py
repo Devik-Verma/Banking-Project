6e932c44bdce14d82918ec57e135eb6a1c4e0b82
Devik Verma <09devikverma@gmail.com>
2026-02-15 13:20:25 +0530

    Updated mailing segment with placeholder email

--- mailing.py
+++ mailing.py
@@ -3,25 +3,25 @@ from email.message import EmailMessage
 
 def sendmail1(name,mail,money):
     email = EmailMessage()
-    email['From'] = "09devikverma@gmail.com"
+    email['From'] = "your mail"
     email['To'] = f"{mail}"
     email['Subject'] = "Test Mail"
     email.set_content(f"Hello {name},an amount of {money} has been deposited to your account")
     
     server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login("09devikverma@gmail.com", "ybaylgbmryqmewpx")
+    server.login("your mail", "your app password")
     server.send_message(email)
     server.quit()
 
 def sendmail2(name,mail,money):
     email = EmailMessage()
-    email['From'] = "09devikverma@gmail.com"
+    email['From'] = "your mail"
     email['To'] = f"{mail}"
     email['Subject'] = "Test Mail"
     email.set_content(f"Hello {name},an amount of {money} has been withdrawn from your account")
     
     server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
-    server.login("09devikverma@gmail.com", "ybaylgbmryqmewpx")
+    server.login("your mail", "your app password")
     server.send_message(email)
     server.quit()
 
